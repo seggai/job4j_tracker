789a73cb77aa96f56b6d8e78a6dc6164259e7fe7
Admin <yuri.argunov@gmail.com>
2020-06-06 00:50:28 +0300

    Creating object

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+/**
+ * @author Yuri.Argunov
+ * @version 1.0
+ * @since 06.06.2020
+ */
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
