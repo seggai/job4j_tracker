e654ff2e31d84fe1f15c711e2f6665fb747acf00
seggai <yuri.argunov@gmail.com>
2020-06-07 02:02:12 +0300

    Object field

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -6,8 +6,31 @@ package ru.job4j.oop;
  * @since 06.06.2020
  */
 public class Cat {
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Polkan");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Barsik");
+        black.eat("fish");
+        black.show();
     }
 }
