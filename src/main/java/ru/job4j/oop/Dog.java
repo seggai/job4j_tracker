f3d8604e75728e7f66dfe0cf4b77cc063dc3ab4c
seggai <yuri.argunov@gmail.com>
2020-08-22 22:29:22 +0300

    Create object

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+/**
+ * @author Yuri.Argunov
+ * @version 1.0
+ * @since 06.06.2020
+ */
+public class Dog {
+    public static void main() {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
