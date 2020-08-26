59d9295f0612eb06b3c45a7def58e7da2a6fc078
Admin <yuri.argunov@gmail.com>
2020-08-27 00:48:10 +0300

    Refactor Point

---
+++ src/main/java/ru/job4j/oop/Point.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+/**
+ * @author Yuri.Argunov
+ * @version 1.0
+ * @since 27.08.2020
+ */
+public class Point {
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+    }
+}
