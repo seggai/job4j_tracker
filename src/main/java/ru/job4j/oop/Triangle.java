f31dff0293ff505f971cd4a805dce95497ef87fe
Admin <yuri.argunov@gmail.com>
2020-09-01 00:23:44 +0300

    Refactor Triangle

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,40 @@
+package ru.job4j.oop;
+
+/**
+ * @author Yuri.Argunov
+ * @version 1.0
+ * @since 27.08.2020
+ */
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+
+    public boolean exist(double ab, double ac, double bc) {
+        return ((ab + ac) > bc) && ((ac + bc) > ab) && ((ab + bc) > ac);
+
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+}
