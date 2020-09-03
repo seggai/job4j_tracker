a05db05f4599f0afbe5a99d69b8de8da72a4b105
Admin <yuri.argunov@gmail.com>
2020-09-04 00:25:57 +0300

    constructor overloading

---
+++ src/main/java/ru/job4j/oop/Item.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+/**
+ * @author Yuri.Argunov
+ * @version 1.0
+ * @since 03.09.2020
+ */
+public class Item {
+    private int id;
+
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(int id) {
+    }
+
+    public Item(String name, int id) {
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+    }
+}
