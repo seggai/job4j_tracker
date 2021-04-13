caa15749a88a83f795af6f0ad2394f4f9b367d6b
seggai <yuri.argunov@gmail.com>
2021-04-13 12:00:00 +0300

    [#254696] reboot const

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,26 +3,17 @@ package ru.job4j.tracker;
 import java.util.Objects;
 
 public class Item {
-    private String id;
+    private int id;
     private String name;
 
-    public Item(String name) {
-        this.name = name;
+    public Item() {
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
+   public Item(int id){
         this.id = id;
-    }
+   }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Item(int id, String name) {
+        this.id = id;
         this.name = name;
     }
 }
