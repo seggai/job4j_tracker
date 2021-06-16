df8cd309fec7ad0bfd7ca6feef97adc35390da8b
seggai <yuri.argunov@gmail.com>
2021-06-16 11:38:22 +0300

    Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String fullname;
+    private String group;
+    private Date created;
+
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
