1cbd4b1ee630c5abd92b73743d45b45b78f8e10c
seggai <yuri.argunov@gmail.com>
2021-06-22 17:13:40 +0300

    date [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,16 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 
     public Item() {
     }
