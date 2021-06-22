91065e11f8142d45b5fcc62887b7956d0f96a56d
seggai <yuri.argunov@gmail.com>
2021-06-22 13:44:33 +0300

    commnet empty classes

--- src/main/java/ru/job4j/prof/Dentist.java
+++ src/main/java/ru/job4j/prof/Dentist.java
@@ -1,19 +1,19 @@
-package ru.job4j.prof;
-
-public class Dentist extends Doctor{
-    private String pacient;
-    private String diagnosis;
-
-    public newDentist(String name, String surname, String education, String birthday, int experience, String certification, String pacient, String diagnosis){
-        super(name, surname, education, birthday, experience, certification);
-        this.diagnosis = diagnosis;
-        this.pacient = pacient;
-    }
-    public getPacient(String pacient){
-        this.pacient = pacient;
-    }
-    public getDiagnosis(String diagnosis){
-        this.diagnosis = diagnosis;
-    }
-
-}
+//package ru.job4j.prof;
+//
+//public class Dentist extends Doctor{
+//    private String pacient;
+//    private String diagnosis;
+//
+//    public newDentist(String name, String surname, String education, String birthday, int experience, String certification, String pacient, String diagnosis){
+//        super(name, surname, education, birthday, experience, certification);
+//        this.diagnosis = diagnosis;
+//        this.pacient = pacient;
+//    }
+//    public getPacient(String pacient){
+//        this.pacient = pacient;
+//    }
+//    public getDiagnosis(String diagnosis){
+//        this.diagnosis = diagnosis;
+//    }
+//
+//}
--- src/main/java/ru/job4j/prof/Surgeon.java
+++ src/main/java/ru/job4j/prof/Surgeon.java
@@ -1,18 +1,18 @@
-package ru.job4j.prof;
-
-public class Surgeon extends Doctor{
-    private String pacient;
-    private String diagnosis;
-
-    public newSurgeon(String name, String surname, String education, String birthday, String certification, int expirience, String pacient, String diagnosis){
-        super(name, surname, education,birthday,certification,expirience);
-        this.pacient = pacient;
-        this.diagnosis = diagnosis;
-    }
-    public getPacient(String pacient){
-        this.pacient = pacient;
-    }
-    public getDiagnosis(String diagnosis){
-        this.diagnosis = diagnosis;
-    }
-}
+//*//package ru.job4j.prof;
+//
+//public class Surgeon extends Doctor{
+//    private String pacient;
+//    private String diagnosis;
+//
+//    public newSurgeon(String name, String surname, String education, String birthday, String certification, int expirience, String pacient, String diagnosis){
+//        super(name, surname, education,birthday,certification,expirience);
+//        this.pacient = pacient;
+//        this.diagnosis = diagnosis;
+//    }
+//    public getPacient(String pacient){
+//        this.pacient = pacient;
+//    }
+//    public getDiagnosis(String diagnosis){
+//        this.diagnosis = diagnosis;
+//    }
+//}
