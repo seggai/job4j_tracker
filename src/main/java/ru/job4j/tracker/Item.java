8fae03898fa9dbbcbaea1bcfcea9aef652181a5f
seggai <yuri.argunov@gmail.com>
2021-06-25 10:45:08 +0300

    toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -44,4 +44,13 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created +
+                '}';
+    }
 }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,11 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item one = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(one.getCreated().format(formatter));
+        Tracker voodoo = new Tracker();
+        Item one = new Item("Vasya");
+        voodoo.add(one);
+        System.out.println(one);
     }
 }
