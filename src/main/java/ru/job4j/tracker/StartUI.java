9749a729d71652ee7ecacaed6efaa12b588fcc63
seggai <yuri.argunov@gmail.com>
2021-06-28 16:19:40 +0300

    Реализация класса StartUI [#784]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,39 +1,39 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StartUI {
 
     public void init(Scanner scanner, Tracker tracker) {
-        boolean run = false;
+        boolean run = true;
         while (run) {
             this.showMenu();
             System.out.print("Select: ");
-            int select = Integer.valueOf(scanner.nextLine());
+            int select = Integer.parseInt(scanner.nextLine());
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
+                System.out.println("=== Create a new Item ===");
                 System.out.print("Enter name: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
+                System.out.println("Created element is: " + item);
             } else if (select == 1) {
-                System.out.println("=== Showing all items ====");
+                System.out.println("=== Showing all items ===");
                 Item[] inputArray = tracker.findAll();
-                System.out.println(inputArray);
-            } else if (select == 2) {
-                System.out.println("=== Edit Item ====");
-                boolean one = true;
-                int id = 0;
-                while (one) {
-                    System.out.println("Enter id of changed item:");
-                    id = Integer.valueOf(scanner.nextLine());
-                    if (id > 0) {
-                        one = !one;
-                    } else {
-                        System.out.println("Invalid id");
+                if (inputArray.length > 0) {
+                    for (Item example : inputArray) {
+                        System.out.println(example);
                     }
+                } else {
+                    System.out.println("Tracker is empty");
                 }
-                System.out.println("Enter the name of the replaced item");
+            } else if (select == 2) {
+                System.out.println("=== Edit Item ===");
+                int id = 0;
+                System.out.println("Enter id of changed item: ");
+                id = Integer.parseInt(scanner.nextLine());
+                System.out.println("Enter the name of the replaced item: ");
                 String name = scanner.nextLine();
                 Item newItem = new Item(name);
                 tracker.replace(id, newItem);
@@ -42,11 +42,45 @@ public class StartUI {
                 } else {
                     System.out.println("Failure");
                 }
-
+            } else if (select == 3) {
+                System.out.println("=== Delete Item ===");
+                int id = 0;
+                System.out.println("Enter id of deleting item: ");
+                id = Integer.parseInt(scanner.nextLine());
+                if (tracker.delete(id)) {
+                    tracker.delete(id);
+                    System.out.println("Success");
+                } else {
+                    System.out.println("Failure");
+                }
+            } else if (select == 4) {
+                int id = 0;
+                System.out.println("=== Find item by Id ===");
+                System.out.println("Enter the id of the element you want to find: ");
+                id = Integer.parseInt(scanner.nextLine());
+                Item foundItem = tracker.findById(id);
+                if (foundItem == null) {
+                    System.out.println("Item with current id not found");
+                } else {
+                    System.out.println(foundItem);
+                }
+            } else if (select == 5) {
+                System.out.println("=== Find items by name ===");
+                System.out.println("Enter a name of elements you want to find: ");
+                String name = scanner.nextLine();
+                Item[] array = tracker.findByName(name);
+                if (array.length > 0) {
+                    for (Item example : array) {
+                        System.out.println(example);
+                    }
+                } else {
+                    System.out.println("Items with current name not found");
+                }
             } else if (select == 6) {
                 run = false;
             }
         }
+
     }
 
     private void showMenu() {
