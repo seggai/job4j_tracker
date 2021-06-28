6f5a7bcf32eb1e2c5cecf7a7512e59672cbfb6b5
seggai <yuri.argunov@gmail.com>
2021-06-28 17:41:44 +0300

    Интерфейс Input [#181092 #172354]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,10 +3,17 @@ package ru.job4j.poly;
 public class Bus implements Transport{
     @Override
     public void passengers(int numbers) {
+        int space = 70;
+        if (space - numbers > 0) {
+            System.out.println("Enough space for passengers");
+        } else {
+            System.out.println("Too much passengers");
+        }
     }
 
     @Override
     public void ride() {
+        System.out.println("London to Moscow");
 
     }
 
