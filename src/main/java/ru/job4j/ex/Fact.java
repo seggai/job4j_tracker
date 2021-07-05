4a404e1a159fedd1b1d90fa5a8c40b9f3de9469f
seggai <yuri.argunov@gmail.com>
2021-07-05 16:32:46 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class Fact {
+
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("'n' must be more than zero");
+        } else {
+            int rsl = 1;
+            for (int index = 1; index <= n; index++) {
+                rsl *= index;
+            }
+            return rsl;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
