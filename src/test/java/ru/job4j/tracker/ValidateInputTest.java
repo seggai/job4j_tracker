e172b9c09f70ac32f20614e3dcd09bf442cdf238
seggai <yuri.argunov@gmail.com>
2021-07-07 18:13:59 +0300

    validatetest process

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -16,4 +16,29 @@ public class ValidateInputTest {
         int selected = input.askInt("Enter menu:");
         assertThat(selected, is(1));
     }
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput() ;
+        Input in = new StubInput( new String[] {"1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+
+    }
+    @Test
+    public void whenMultipleValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput( new String[] {"1", "1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+    @Test
+    public void whenNegativeNumberInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput( new String[] {"-1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-1));
+    }
 }
